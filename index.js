ea34471fa4e4aae8db22d5def51a0fd25be38776
TGDarLoW <darlow.the.master@gmail.com>
2022-02-18 14:48:52 +0100

    test2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
